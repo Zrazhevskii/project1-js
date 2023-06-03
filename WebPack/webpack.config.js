0c11fb94bdd71eec1244569f0d00d8c0bb8eabf2
Zrazhevskii <vbn-134@yandex.ru>
2023-06-03 17:13:15 +0300

    добавление HtmlWebpackPlugin

--- WebPack/webpack.config.js
+++ WebPack/webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { Template } = require('webpack');
 
 module.exports = {
     module: {
         rules: [
-            
+
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin ({
+            template: './src/index.html'
+        })
+    ]
 }
