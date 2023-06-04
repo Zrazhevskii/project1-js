d0dd20534e5ac5a171fd2f3c395ed518cc62e002
Zrazhevskii <vbn-134@yandex.ru>
2023-06-04 14:06:15 +0300

    отладка Eslint

--- WebPack/src/js/app.js
+++ WebPack/src/js/app.js
@@ -1,6 +1,10 @@
-import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from "./game";
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game';
 
 const game = new Game();
 game.start();
 
 console.log('app worked');
+
+const gameSavingData = new GameSavingData();
+loadGame();
+saveGame();
--- WebPack/src/js/game.js
+++ WebPack/src/js/game.js
@@ -1,16 +1,13 @@
-import Character from './domain.js'
+import Character from './domain.js';
 
 export default class Game {
-    start() {
-      console.log('game started');
-    }
+  start() {
+    console.log('game started');
   }
-  
-  export class GameSavingData {
-  }
-  
-  export function readGameSaving() {
-  }
-  
-  export function writeGameSaving() {
-  }
+}
+
+export class GameSavingData {}
+
+export function readGameSaving() {}
+
+export function writeGameSaving() {}
