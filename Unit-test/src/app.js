02164781e41c1de0cfe30202ab0dc92d26425397
Zrazhevskii <vbn-134@yandex.ru>
2023-06-14 14:57:13 +0300

    изменение функции

--- Unit-test/src/app.js
+++ Unit-test/src/app.js
@@ -1,7 +1,5 @@
 export function calcTotal(lists) {
-    let result = 0;
-    for (let i = 0; i < lists.length; i++) {
-        result += lists[i].count * lists[i].price
-    }
-    return result
+    return lists.reduce((total, item) => {
+        return total + (item.count * item.price);
+    }, 0)
 }
