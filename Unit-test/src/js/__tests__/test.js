aef9ecc46503683897eea92e8f8e37c103411170
Zrazhevskii <vbn-134@yandex.ru>
2023-06-14 20:44:07 +0300

    Написание тестов

--- Unit-test/src/js/__tests__/test.js
+++ Unit-test/src/js/__tests__/test.js
@@ -2,8 +2,32 @@ import streakOfLife from '../health.js'
 
 test('check health: 40', () => {
     const list = streakOfLife({
-        name: 'Маг',
+        name: 'Лучник',
         health: 40
     })
     expect(list).toBe('wounded')
+})
+
+test('check health: 70', () => {
+    const list = streakOfLife({
+        name: 'Маг',
+        health: 70
+    })
+    expect(list).toBe('healthy')
+})
+
+test('check health: 12', () => {
+    const list = streakOfLife({
+        name: 'Пехотинец',
+        health: 12
+    })
+    expect(list).toBe('critical')
+})
+
+test('check health: -10', () => {
+    const list = streakOfLife({
+        name: 'Зомби',
+        health: -10
+    })
+    expect(list).toBe('Error')
 })
--- Unit-test/src/js/health.js
+++ Unit-test/src/js/health.js
@@ -1,11 +1,12 @@
 export default function streakOfLife(item) {
     if (item.health > 50) {
         return 'healthy'
-    } else if (item.health <= 50 && item.health > 15) {
+    } 
+    if (item.health <= 50 && item.health > 15) {
         return 'wounded'
-    } else if (item.health <= 15 && item.health > 0) {
+    } 
+    if (item.health <= 15 && item.health >= 0) {
         return 'critical'
-    } else {
-        alert('Что-то пошло не так')
-    }
+    } 
+    return 'Error'
 }
