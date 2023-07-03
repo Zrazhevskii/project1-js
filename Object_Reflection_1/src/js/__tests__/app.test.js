4db77265699355b3bcc2d5e32d60e2139b03d2c5
Zrazhevskii <vbn-134@yandex.ru>
2023-07-03 10:52:34 +0300

    дописание функции, отработка теста

--- Object_Reflection_1/src/js/__tests__/app.test.js
+++ Object_Reflection_1/src/js/__tests__/app.test.js
@@ -1,9 +1,41 @@
 import orderByProps from '../app';
 
-const obj = {
-    name: 'мечник',
-    health: 10,
-    level: 2,
-    attack: 80,
-    defence: 40,
-  };
+
+test.each([
+    [
+        ['level', 'name'], [
+            { key: 'level', value: 2 },
+            { key: 'name', value: 'мечник' },
+            { key: 'attack', value: 80 },
+            { key: 'defence', value: 40 },
+            { key: 'health', value: 10 },
+        ],
+    ],
+    [
+        ['name', 'level'], [
+            { key: 'name', value: 'мечник' },
+            { key: 'level', value: 2 },
+            { key: 'attack', value: 80 },
+            { key: 'defence', value: 40 },
+            { key: 'health', value: 10 },
+        ],
+    ],
+    [
+        ['health', 'defence', 'name'], [
+            { key: 'health', value: 10 },
+            { key: 'defence', value: 40 },
+            { key: 'name', value: 'мечник' },
+            { key: 'attack', value: 80 },
+            { key: 'level', value: 2 },
+        ],
+    ],
+])('тест на корректную сортировку по указываемым данным', (dataToSort, expected) => {
+    const obj = {
+        name: 'мечник',
+        health: 10,
+        level: 2,
+        attack: 80,
+        defence: 40,
+    };
+    expect(orderByProps(obj, dataToSort)).toEqual(expected);
+});
--- Object_Reflection_1/src/js/app.js
+++ Object_Reflection_1/src/js/app.js
@@ -1,3 +1,15 @@
-export default function orderByProps(obj) {
+export default function orderByProps(obj, dataToSort) {
+    const arrOrder = [];
+    const data = [];
 
+    for (const key in obj) {
+        if (dataToSort.includes(key)) {
+            const index = dataToSort.indexOf(key);
+            arrOrder.splice(index, 0, { key, value: obj[key] });
+        } else {
+            data.push({ key, value: obj[key] });
+        }
+    }
+    data.sort((a, b) => (a.key > b.key ? 1 : -1));
+    return [...arrOrder, ...data];
 }
