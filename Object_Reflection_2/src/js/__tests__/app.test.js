62b077db68d5ed1d8a6a3128cb4df5e659044327
Zrazhevskii <vbn-134@yandex.ru>
2023-07-04 18:47:00 +0300

    написание функции теста

---
+++ Object_Reflection_2/src/js/__tests__/app.test.js
@@ -0,0 +1,40 @@
+import attackOptions from '../app';
+
+test('Проверка работы функции', () => {
+
+    expect(attackOptions({
+        name: 'Лучник',
+        type: 'Bowman',
+        health: 50,
+        level: 3,
+        attack: 40,
+        defence: 10,
+        special: [
+          {
+            id: 8,
+            name: 'Двойной выстрел',
+            icon: 'http://...',
+            description: 'Двойной выстрел наносит двойной урон',
+          },
+          {
+            id: 9,
+            name: 'Нокаутирующий удар',
+            icon: 'http://...',
+            // <- обратите внимание, описание "засекречено"
+          },
+        ],
+      })).toEqual([
+        {
+          id: 8,
+          name: 'Двойной выстрел',
+          icon: 'http://...',
+          description: 'Двойной выстрел наносит двойной урон',
+        },
+        {
+          id: 9,
+          name: 'Нокаутирующий удар',
+          icon: 'http://...',
+          description: 'Описание недоступно',
+        },
+      ]);
+  }) 
