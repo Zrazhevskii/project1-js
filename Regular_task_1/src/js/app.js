d60cc81591749b4b514725583cdb9bf8f5fa22ed
Zrazhevskii <vbn-134@yandex.ru>
2023-07-11 13:01:55 +0300

    написание теста

--- Regular_task_1/src/js/app.js
+++ Regular_task_1/src/js/app.js
@@ -3,14 +3,13 @@ export default class Validator {
         this.name = name;
     }
 
-
     validateUsername(name) {
-      if (!/^([^?!\d-_])([\w-_]+)([^?!\d-_])$/i.test(name) || /\d{4,}/.test(name)) {
-        throw new Error ('Не корректное имя, просьба посмотреть требования к имени')
-      }
-      return 'Поздравляем вы зарегестрированы!'  
-    } 
+        if (
+            !/^([^?!\d-_])([\w-_]+)([^?!\d-_])$/i.test(name) ||
+            /\d{4,}/.test(name)
+        ) {
+            return 'Не корректное имя, просьба посмотреть требования к имени'
+        }
+        return 'Поздравляем вы зарегестрированы!';
+    }
 }
-
-let str = new Validator;
-console.log(str.validateUsername('Ser44gei'));
