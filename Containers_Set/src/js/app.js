0b83cadf6b93504c3aabd0df8e4a8e7ef37527a9
Zrazhevskii <vbn-134@yandex.ru>
2023-07-15 12:02:21 +0300

    исправление ошибок lint

--- Containers_Set/src/js/app.js
+++ Containers_Set/src/js/app.js
@@ -1,27 +1,21 @@
 export default class Team {
     constructor() {
-      this.members = new Set();
+        this.members = new Set();
     }
-  
-    add(name) {
-      if (this.members.has(name)) {
-        throw new Error('Такой персонаж уже есть');
-      }
-      this.members.add(name);
-    }
-  
-    addAll(...rest) {
-      rest.forEach((member) => this.members.add(member));
-      return this.members
-    }
-  
-    toArray() {
-      return Array.from(this.members);
-    }
-  }
 
-// const arr = new Team();
-// console.log(arr.addAll('piton', 'hueton'));
-// console.log(arr.addAll('zaraza', 'hueton'));
-// console.log(arr.add('huetons'));
-// console.log(arr.toArray());
+    add(name) {
+        if (this.members.has(name)) {
+            throw new Error('Такой персонаж уже есть');
+        }
+        this.members.add(name);
+    }
+
+    addAll(...rest) {
+        rest.forEach((member) => this.members.add(member));
+        return this.members;
+    }
+
+    toArray() {
+        return Array.from(this.members);
+    }
+}
