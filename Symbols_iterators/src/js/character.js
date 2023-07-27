0cdb2d2f3baa7532324a1168788a3075f9a8c299
Zrazhevskii <vbn-134@yandex.ru>
2023-07-27 13:03:43 +0300

    добавление классов

---
+++ Symbols_iterators/src/js/character.js
@@ -0,0 +1,33 @@
+export default class Character {
+    constructor(name, type) {
+      const types = [
+        'Bowman',
+        'Swordsman',
+        'Magician',
+        'Daemon',
+        'Undead',
+        'Zombie',
+      ];
+  
+      if (name.length < 2 || name.length > 10) {
+        throw new Error(
+          'Недопустимая длина имена, количество букв должно быть от 2 до 10',
+        );
+      } else {
+        this.name = name;
+      }
+  
+      if (!types.includes(type)) {
+        throw new Error(
+          `Неизвестный тип игрока. Допустипые типы: ${types}`,
+        );
+      } else {
+        this.type = type;
+      }
+  
+      this.health = 100;
+      this.level = 1;
+      this.attack = undefined;
+      this.defense = undefined;
+    }
+  }
